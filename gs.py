5e26d0b1766c1ac310a09a115aa1f2bdcfe003e1
Ivan Ershov <ershov.iv14@gmail.com>
2023-09-19 16:11:44 +0500

    New gs.py | Work mach faster

--- gs.py
+++ gs.py
@@ -11,39 +11,64 @@ wrk_guide = sh.worksheet(REFERENCE)
 wrk_data = sh.worksheet(VAULT)
 
 
-# Возвращает словарь, где k - Ф.И. менеджера, v - словарь(k - название параметра, v - значение в конкретный день)
-def get_all_managers_data(date):
+def get_data(date_list):
 
-    parameters_dict = {}
-    for parameter in PARAMETERS:
-        parameters_dict[parameter] = wrk_data.find(parameter).row-wrk_data.find(BEGINNING).row+1
-
+    data = wrk_data.get_all_values()
     managers_list = wrk_guide.col_values(1)[1:]
-    
-    all_managers_data = {}
+
+    head_cell_row = wrk_data.find(START_CELL).row-1
+    data_dict = {}
+    len_of_row = len(data[0])
+
+    for col_num in range(len_of_row): # Данные с гугл таблици форматируются в словар (k - название столбца, v - список со всеми значениями)
+        col_value_list = []
+        for row in data:
+            col_value_list.append(row[col_num])
+        key_name = data[head_cell_row][col_num]
+        data_dict[key_name] = col_value_list
+
+    heads_list = data_dict[START_CELL]
+    managers_data = {}
 
     for manager in managers_list:
-        start_cell = wrk_data.find(f"{manager} start")
-        date_cell = wrk_data.find(f"{date}")
-        manager_data = {}
-        for parameter, delta in parameters_dict.items():
-            manager_data[parameter] = wrk_data.cell(start_cell.row+delta, date_cell.col).value
-        all_managers_data[manager] = manager_data
+        one_manager_data = {}
+        manager_start = heads_list.index(f'{manager} start')
+        manager_end = heads_list.index(f'{manager} end')
+        
+        for parameter in PARAMETERS:
+            for head_num in range(manager_start, manager_end):
+                if heads_list[head_num] == parameter:
+                    one_manager_data[parameter] = 0
+                    for day in date_list:
+                        values_on_date_list = data_dict[day]
+                        one_manager_data[parameter] += int(values_on_date_list[head_num])
+        managers_data[manager] = one_manager_data
+    
+    return(managers_data)
 
-    return(all_managers_data)
 
+def make_report(date_list):
 
-# Возвращает отформатированный отчет одной строкой, используя словарь из функции get_all_managers_data()
-def make_today_report():
+    managers_data = get_data(date_list)
+    if len(date_list) == 1:
+        report = f"\n\tРезультаты за {date_list[0]}" # Форматирует ежедневный отчет в одну строку
+    else:
+        report = f"\n\tРезультаты за {date_list[0]} - {date_list[-1]}"
 
-    report_date = datetime.datetime.today().strftime('%d.%m') # Записывает актуальную дату в переменную в формате ДД.ММ
-    data_for_report = get_all_managers_data(report_date)
-    report = f"\t{report_date}\n"
-
-    for manager, data in data_for_report.items():
-        report += f"\n{manager}"
-        for parametr, value in data.items():
-            report += f"\n{parametr}: {value}"
-        report += f"\n"
+    for manager_name, manager_values in managers_data.items():
+        report += f"\n\n{manager_name}"
+        for parameter_name, parameter_value in manager_values.items():
+            report += f"\n{parameter_name}: {parameter_value}"
     
     return(report)
+
+
+def make_today_report():
+    today_date = []
+    today_date.append(datetime.datetime.today().strftime('%d.%m'))
+    report = make_report(today_date)
+    return(report)
+
+
+
+
