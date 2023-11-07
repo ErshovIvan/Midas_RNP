b568ab272c364017f234aca20deb76cdb7aee8da
Ershov Ivan <ershov.iv14@gmail.com>
2023-11-07 16:46:50 +0500

    Test super group

--- handlers/hd_gs_report.py
+++ handlers/hd_gs_report.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from config import CHAT_ID
+from config import CHAT_ID, S_GROUP_ID, S_GROUP_THREAD
 
 import gs
 from texts import t_admin_handlers, t_gs_report, t_system
@@ -64,3 +64,14 @@ async def send_daily_report_to_chat(bot: Bot):
 async def send_weekly_report_to_chat(bot: Bot):
     """ Выводит отчет РНП за последние 7 дней в групповой чат. """
     await bot.send_message(CHAT_ID, gs.make_last_week_report())
+
+
+# Тест
+async def send_daily_report_to_s_group(bot: Bot):
+    """ Выводит сегодняшний отчет РНП в супер группу. """
+    await bot.send_message(chat_id=S_GROUP_ID, message_thread_id=S_GROUP_THREAD, text=gs.make_today_report())
+
+
+async def send_weekly_report_to_s_group(bot: Bot):
+    """ Выводит отчет РНП за последние 7 дней в супер группу. """
+    await bot.send_message(chat_id=S_GROUP_ID, message_thread_id=S_GROUP_THREAD, text=gs.make_today_report())
